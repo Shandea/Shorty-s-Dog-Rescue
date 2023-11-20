a7642013ecacf14cec39e72a9a364a58ac6fbad1
okrayum <morgannj71@gmail.com>
2023-11-20 13:55:34 -0700

    navbar component and file structure updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './styles/App.css';
+import NavBar from "./components/NavBar";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Adopt from "./pages/adopt/Adopt";
+import Donate from "./pages/donate/Donate";
 
 
 function App() {
   return (
     <div className="App">
-      Shorty's Dog Rescuce
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/adopt" element={<Adopt />} />
+        <Route path="/donate" element={<Donate />} />
+      </Routes>
     </div>
   );
 }
