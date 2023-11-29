aba039abf0cf6b3fd99124be1509d994a573c7d8
okrayum <morgannj71@gmail.com>
2023-11-29 10:01:41 -0700

    server folder set up

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require("cors");
+const bodyParser = require("bodyParser");
+const router = require("./routes/router")
+
+const app = express();
+
+// Define routes and middleware here
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccsessStatus: 200
+
+}
+
+app.use(cors(corsOptions));
+app.use("/", router)
+
+
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+// const server = app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
