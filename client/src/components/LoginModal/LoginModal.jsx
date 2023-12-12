b2c3bc16ec5b3d849b2567ccc2ea278627778b5b
okrayum <morgannj71@gmail.com>
2023-12-12 10:17:15 -0700

    added client folder to file structure

---
+++ client/src/components/LoginModal/LoginModal.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import adminImg from '../../assets/blockAdmin.jpg';
+import './LoginModal.css';
+
+const LoginModal = ({ isAdmin, setIsAdmin, setShowLogIn, setLogOut, setIsOpen }) => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const bg = {
+        backgroundColor: '#fff3e6'
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (username && password === 'admin') {
+            setIsAdmin(true)
+            setLogOut(true)
+            setShowLogIn(false)
+            setIsOpen(false)
+        }
+    }
+
+    const closeModal = () => {
+        setLogOut(false)
+        setShowLogIn(true)
+        setIsOpen(false)
+    }
+
+    return (
+        <div className="modal-background">
+            <form className="modal">
+                <div className="imgcontainer">
+                    <img src={adminImg} alt="Avatar" className="avatar" />
+                </div>
+                <div className="container">
+                    <label htmlFor="username"><b>Username</b></label>
+                    <input
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        className="input" type="text" placeholder="Username" name="username"
+                        required
+                    />
+                    <label htmlFor="psw"><b>Password</b></label>
+                    <input
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} className="input" type="password" placeholder="Password" name="psw" required />
+                    <button onClick={(e) => handleSubmit(e)} className="login" type="submit">Login</button>
+                </div>
+                <div className="container" style={bg}>
+                    <button onClick={closeModal} type="button" className="cancelbtn">
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginModal
---
+++ client/src/components/card/Card.jsx
@@ -0,0 +1,43 @@
+import { useNavigate } from "react-router-dom";
+import './card.css'
+import petsData from './petsData.json'
+
+const Card = () => {
+
+    const imageStyle = {
+        width: '350px',   // Set the width to 350 pixels
+        height: '350px',  // Set the height to 350 pixels
+    };
+
+    const navigate = useNavigate();
+    const handleButtonClick = () => {
+        navigate("/adopt");
+    }
+
+    return (
+        <div className='flip-card-container'>
+            {petsData.map((pet, index) => (
+                <div key={index} className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <img src={require(`../../assets/${pet.image}`)} alt="Avatar" style={imageStyle} />
+                        </div>
+                        <div className="flip-card-back">
+                            <h1>{pet.name}</h1>
+                            <p>{pet.breed}</p>
+                            <p>Age: {pet.age}</p>
+                            <p>Gender: {pet.gender}</p>
+                            <small>{pet.description}
+                            </small>
+                            <br />
+                            <div className='btn-container'>
+                                <button className='pickMe-buttons' onClick={handleButtonClick}>Pick Me</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+export default Card
